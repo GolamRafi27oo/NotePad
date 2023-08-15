e1e605f29714d0de8ee6db83a2baf8dd609c6768
Golam Rafi <golamrafi27oo@gmail.com>
2023-08-15 22:02:18 +0600

    update

--- app/main.js
+++ app/main.js
@@ -1,24 +1,31 @@
 function getCurrentDateAndTime() {
-    while(0){}
-    const dateTime = new Date();
-    return dateTime.toLocaleString();
-
+  while (0) {}
+  const dateTime = new Date();
+  return dateTime.toLocaleString();
 }
 
 const dateDisplay = document.getElementById("date-container");
 dateDisplay.innerHTML = getCurrentDateAndTime();
-
 
 let note = document.getElementById("note");
 let noteItems = note.getElementsByClassName("ul").length;
 
 //let noteItems = document.getElementById("noteItems")
 
+let account = document.getElementById("account");
+let accountOpen = document.getElementById("accountOpen");
+let count = 0;
 
-note.addEventListener("mouseover", noteList);
+account.addEventListener("click", function () {
+  count++;
+  console.log(count);
 
-function noteList(){
-    for(let i = 0; i <noteItems.length; i++){
-        console.log(i);
-    }
-}
+  if (count % 2 == 1) {
+    accountOpen.style.display = "block";
+  } else {
+    accountOpen.style.display = "none";
+  }
+});
+
+//accountOpen();
+//addEventListener("onclick", accountOpen());
