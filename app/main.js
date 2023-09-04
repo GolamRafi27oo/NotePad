fa180cb3672451568e358a6c36ca73d343fa03d4
Golam Rafi <golamrafi27oo@gmail.com>
2023-09-04 21:23:41 +0600

    .

--- app/main.js
+++ app/main.js
@@ -1,5 +1,4 @@
 function getCurrentDateAndTime() {
-  while (0) {}
   const dateTime = new Date();
   return dateTime.toLocaleString();
 }
@@ -22,10 +21,28 @@ account.addEventListener("click", function () {
 
   if (count % 2 == 1) {
     accountOpen.style.display = "block";
+    account.style.color = "black"
+    accountOpen.classList.add("slideOut");
   } else {
+    account.style.color = "#edbf1a"
+    accountOpen.classList.add("slide-in");
     accountOpen.style.display = "none";
+    
   }
 });
 
+let sidebar = document.getElementById("sidebar");
+
+let sidebarCount = 0;
+sidebar.addEventListener("click",function (){
+  sidebarCount++;
+  if (sidebarCount % 2 == 1) {
+    let leftSideID = document.getElementById("leftSide");
+    leftSideID.style.display = "block";
+  } else {
+    let leftSideID = document.getElementById("leftSide");
+    leftSideID.style.display = "none";
+  }
+})
 //accountOpen();
 //addEventListener("onclick", accountOpen());
